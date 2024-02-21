6c07f8c13886dae7f243d67a72f45d693522561e
simoneevacca <v.simone.vacca@gmail.com>
2024-02-21 15:11:47 +0100

    add content of value on html ducument

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,24 +3,20 @@ let elementList = document.getElementById('list');
 
 let value = 0;
 for (let i = 1; i <= 100; i++) {
-    value ++;
-    
+    value++;
+
     if (value % 3 == 0 && value % 5 == 0) {
         console.log('FizzBuzz');
-        
+        elementList.insertAdjacentHTML('beforebegin', `<li>${'FizzBuzz'}</li>`);
+
     } else if (value % 3 == 0) {
         console.log('Fizz');
-        
+        elementList.insertAdjacentHTML('beforebegin', `<li>${'Fizz'}</li>`);
+    
     } else if (value % 5 == 0) {
         console.log('Buzz');
-        
-    } else {
-        console.log(value);
-        
-
+        elementList.insertAdjacentHTML('beforebegin', `<li>${'Buzz'}</li>`);
     }
-
-    // console.log(i);
-
-    // elementList.insertAdjacentHTML('beforebegin', `<li>${i}</li>`);
+    console.log(value);
+    elementList.insertAdjacentHTML('beforebegin', `<li>${value}</li>`);
 }
